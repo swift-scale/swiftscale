80216eb89dbe1a83cc692c13a64f75d7dc981078
Shanky43 <shankartrailmail@gmail.com>
2026-03-14 12:25:59 +0530

    feat:backend-setup done

--- models/User.ts
+++ models/User.ts
@@ -37,15 +37,14 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 // Method to compare password
-userSchema.methods.comparePassword = async function(candidatePassword, userPassword) {
+userSchema.methods.comparePassword = async function(candidatePassword: string, userPassword: string) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
